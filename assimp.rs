74c26ea99a00c06a4d598956a44447331fc71082
Dan Gopstein <dan@gopstein.com>
2015-06-13 16:19:29 -0400

    rename back to assimp

--- assimp.rs
+++ assimp.rs
@@ -9,19 +9,17 @@ pub use consts::*;
 pub mod ffi;
 pub mod consts;
 
-impl Scene {
-	pub fn load( filename: &str, flags: u32 ) -> Result<&ffi::aiScene, String> {
-    let c_filename = std::ffi::CString::new(filename);
-    let err_msg = format!("Could not load file: {}", filename);
+pub fn load( filename: &str, flags: u32 ) -> Result<&ffi::aiScene, String> {
+  let c_filename = std::ffi::CString::new(filename);
+  let err_msg = format!("Could not load file: {}", filename);
 
-    unsafe {
-      let scene_ptr = ffi::aiImportFile( c_filename.unwrap().as_ptr(), flags );
+  unsafe {
+    let scene_ptr = ffi::aiImportFile( c_filename.unwrap().as_ptr(), flags );
 
-      if scene_ptr.is_null() {
-        Err(err_msg)
-      } else {
-        Ok(&*scene_ptr)
-      }
+    if scene_ptr.is_null() {
+      Err(err_msg)
+    } else {
+      Ok(&*scene_ptr)
     }
-	}
+  }
 }
